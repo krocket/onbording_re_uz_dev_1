a86ae822781ef15e6de9cd48125fd08ca5cf7454
Coralie <coralie.lecocq.62@gmail.com>
2025-07-09 15:18:43 +0200

    fix: test

--- reuz_sale_management/models/product_template.py
+++ reuz_sale_management/models/product_template.py
@@ -10,12 +10,10 @@ class ProductTemplate(models.Model):
     color = fields.Char(string='Color')
     technology = fields.Char(string='Technology')
 
-
     api_reference = fields.Char(string='API Reference', compute='_compute_api_reference')
     silver_reference = fields.Char(string='Silver Reference')
-
 
     @api.depends('format', 'volume', 'base', 'color', 'technology')
     def _compute_api_reference(self):
         for record in self:
-            record.display_name = f"{record.format} {record.volume} {record.base} {record.color} {record.technology}"
+            record.api_reference = f"{record.format} {record.volume} {record.base} {record.color} {record.technology}"
