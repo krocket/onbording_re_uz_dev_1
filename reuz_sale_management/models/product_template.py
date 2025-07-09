fbcd7c93cc24bdecf5fad415a3fd5eb5797ebc04
Coralie <coralie.lecocq.62@gmail.com>
2025-07-09 15:14:57 +0200

    feat: add compute

--- reuz_sale_management/models/product_template.py
+++ reuz_sale_management/models/product_template.py
@@ -1,8 +1,21 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     _description = 'Product for reuz'
 
-    api_reference = fields.Char(string='API Reference')
+    format = fields.Char(string='Format')
+    volume = fields.Char(string='Volume')
+    base = fields.Char(string='Base')
+    color = fields.Char(string='Color')
+    technology = fields.Char(string='Technology')
+
+
+    api_reference = fields.Char(string='API Reference', compute='_compute_api_reference')
     silver_reference = fields.Char(string='Silver Reference')
+
+
+    @api.depends('format', 'volume', 'base', 'color', 'technology')
+    def _compute_api_reference(self):
+        for record in self:
+            record.display_name = f"{record.format} {record.volume} {record.base} {record.color} {record.technology}"
