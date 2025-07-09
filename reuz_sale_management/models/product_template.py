9d4fe7f53f1fc4f90a4c4ead9c8ba2da222622aa
Coralie <coralie.lecocq.62@gmail.com>
2025-07-09 15:49:35 +0200

    test

--- reuz_sale_management/models/product_template.py
+++ reuz_sale_management/models/product_template.py
@@ -4,17 +4,17 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
     _description = 'Product for reuz'
 
-    reuz_format = fields.Char(string='Format')
-    reuz_volume = fields.Char(string='Volume')
-    reuz_base = fields.Char(string='Base')
-    reuz_color = fields.Char(string='Color')
-    reuz_technology = fields.Char(string='Technology')
+    reuz_product_format = fields.Char(string='Format')
+    reuz_product_volume = fields.Char(string='Volume')
+    reuz_product_base = fields.Char(string='Base')
+    reuz_product_color = fields.Char(string='Color')
+    reuz_product_technology = fields.Char(string='Technology')
 
-    api_reference = fields.Char(string='API Reference', compute='_compute_api_reference')
-    silver_reference = fields.Char(string='Silver Reference')
+    reuz_api_reference = fields.Char(string='API Reference', compute='_compute_api_reference')
+    reuz_silver_reference = fields.Char(string='Silver Reference')
 
-    @api.depends('reuz_format', 'reuz_volume', 'reuz_base', 'reuz_color', 'reuz_technology')
+    @api.depends('reuz_product_format', 'reuz_product_volume', 'reuz_product_base', 'reuz_product_color', 'reuz_product_technology')
     def _compute_api_reference(self):
         for record in self:
-            record.api_reference = f"{record.reuz_format or ''}{record.reuz_volume or ''}{record.reuz_base or ''}{record.reuz_color or ''}{record.reuz_technology or ''}"
+            record.reuz_api_reference = f"{record.reuz_product_format or ''}{record.reuz_product_volume or ''}{record.reuz_product_base or ''}{record.reuz_product_color or ''}{record.reuz_product_technology or ''}"
       
